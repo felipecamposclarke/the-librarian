272103257d37cc358318a2310b4dac8e642879ce
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2014-11-25 01:29:30 -0300

    chore(): run editorial scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :editorials
   resources :books
 
   get 'home/index'
   root to: 'home#index'
 
   devise_for :users
-  
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
